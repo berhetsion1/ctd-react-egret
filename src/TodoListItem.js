ccbfeb933e6c9862c927323b2e6ebd8d1a8a8ed6
berhetsion1 <78325859+berhetsion1@users.noreply.github.com>
2021-10-26 22:27:48 -0400

    lesson.1.8,lesson.1.9

--- src/TodoListItem.js
+++ src/TodoListItem.js
@@ -1,15 +1,16 @@
-import React from 'react'
-
-function TodoListItem({todo,onRemoveTodo}) {
-    console.log({todo})
+function TodoListItem({ todo, onRemoveTodo }) {
+    console.log({ todo });
     return (
-        <div>
-           <li key={todo.id}>{todo.title}<button  onClick={()=>onRemoveTodo(todo.id)} type="button">Remove</button> </li> 
-
-        </div>
-    )
-}
-
-export default TodoListItem
+      <div>
+        <li >
+          {todo.fields.Title}
+          <button onClick={() => onRemoveTodo(todo.id)} type="button">
+            Remove
+          </button>
+        </li>
+      </div>
+    );
+  }
+  export default TodoListItem;
 
 
